a06512e9a2c17fe18dc6f1baf1369c179dc8fd91
Dominique Elvira <delviratapia@gmail.com>
2022-02-15 22:05:17 -1200

    fixed responsive, added div containers for grid columns

--- src/main.js
+++ src/main.js
@@ -2,21 +2,3 @@
 // import App from './App.vue'
 // import "./assets/styles.css"
 // createApp(App).mount('#app')
-const timeObj = {
-    "txt1": {
-      "tz": "Europe/Berlin",
-      "locale": "de-DE"
-    }
-  };
-
-  const timeClock = () => {
-    for (t in timeObj) {
-      const tObj = timeObj[t];
-      let d = new Date().toLocaleString(
-        tObj.locale, { "timeZone": tObj.tz })
-      document.querySelector("#" + t).innerHTML = d.split(", ")[1];
-    }
-  };
-  setInterval(timeClock, 500);
-
-  
