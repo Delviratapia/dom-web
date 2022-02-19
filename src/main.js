8bb085605d00e75f921f54c538f7fbea5f1d34c8
Dominique Elvira <delviratapia@gmail.com>
2022-02-18 19:24:29 -1200

    - Added links to portfolio preview - Added images - Added javascript to items - Added download link to CV

--- src/main.js
+++ src/main.js
@@ -1,42 +1,54 @@
 const HOVERAUDIO = new Audio("src/assets/audio/rpg_hover-2.wav")
 const USEITEMAUDIO = new Audio("src/assets/audio/rpg_useItem.wav")
-const CONFIRMUDIO = new Audio("src/assets/audio/rpg_confirm.wav")
+const CONFIRMAUDIO = new Audio("src/assets/audio/rpg_confirm.wav")
 const DENIEDAUDIO = new Audio("src/assets/audio/rpg_denied.wav")
-let menuTextAudio = document.querySelectorAll(".cursor-pointer.menu-text")
-let items = document.querySelectorAll(".portfolio-item,.cv-item")
+let menuText = document.querySelectorAll(".cursor-pointer.menu-text")
+let menuItems = document.querySelectorAll(".cursor-pointer.menu-item")
+let menuPreview = document.querySelectorAll(".preview-links")
 
 
-function showPreview (){
-    item.addEventListener("click",() => {
+//General reusable function 
+
+function showPreview() {
+    item.addEventListener("click", () => {
         item.classList.remove("hidden");
     })
 }
 
+// Audio on click
 
-
-for (let btn of menuTextAudio) {
+for (let btn of menuText) {
     btn.addEventListener("click", () => {
         HOVERAUDIO.play()
     })
 
 }
 
+for (let btn of menuItems) {
+    btn.addEventListener("click", () => {
+        USEITEMAUDIO.play()
+    })
 
-for (let item of items) {
-    item.addEventListener("click", () => {
-        USEITEMAUDIO.play();
-        showPreview();
+}
 
+for (let btn of menuPreview) {
+    btn.addEventListener("click", () => {
+        CONFIRMAUDIO.play()
     })
 
 }
 
 
+// for (let item of items) {
+//     item.addEventListener("click", () => {
+//         USEITEMAUDIO.play();
+      
+//     })
+
+// }
 
 
-
-
-
+// portfolio 
 
 let myItems = document.querySelector(".my-items")
 let portfTitle = document.querySelector(".portfolio-title")
@@ -45,15 +57,19 @@ portfTitle.addEventListener("click", () => {
     for (let item of myItems.children) {
         if (item.classList.contains("portfolio-item")) {
             item.classList.remove("hidden");
-        }
-        else {
-            if (!item.classList.contains("hidden")){
+        } else {
+            if (!item.classList.contains("hidden")) {
                 item.classList.add("hidden");
             }
 
         }
     }
 })
+
+
+
+// CV
+
 
 let cvTitle = document.querySelector(".cv-title")
 
@@ -61,12 +77,81 @@ cvTitle.addEventListener("click", () => {
     for (let item of myItems.children) {
         if (item.classList.contains("cv-item")) {
             item.classList.remove("hidden");
-        }
-        else {
-            if (!item.classList.contains("hidden")){
+        } else {
+            if (!item.classList.contains("hidden")) {
                 item.classList.add("hidden");
             }
 
         }
     }
 })
+
+
+// Portfolio PREVIEW
+
+let myItemsPreview = document.querySelector(".my-items-preview")
+let avistaloo = document.querySelector(".avistaloo")
+
+avistaloo.addEventListener("click", () => {
+    for (let item of myItemsPreview.children) {
+        if (item.classList.contains("preview-links")) {
+            item.classList.remove("hidden");
+        } else {
+            if (!item.classList.contains("hidden")) {
+                item.classList.add("hidden");
+            }
+
+        }
+    }
+})
+
+
+// CV Preview
+
+
+let myItemsPreview = document.querySelector(".my-items-preview")
+let avistaloo = document.querySelector(".avistaloo")
+
+avistaloo.addEventListener("click", () => {
+    for (let item of myItemsPreview.children) {
+        if (item.classList.contains("preview-links")) {
+            item.classList.remove("hidden");
+        } else {
+            if (!item.classList.contains("hidden")) {
+                item.classList.add("hidden");
+            }
+
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Portfolio PREVIEW
+
+// let avistaloo = document.querySelector(".avistaloo")
+// let previewContainer = document.querySelectorAll(".preview-container > .preview-links")
+
+// avistaloo.addEventListener("click", () => {
+//         if (previewContainer.classList.contains("hidden")) {
+//             previewContainer.classList.remove("hidden");
+//         } else {
+//             if (!previewContainer.classList.contains("hidden")) {
+//                 previewContainer.classList.add("hidden");
+//             }
+
+//         }
+
+// })
+
+
