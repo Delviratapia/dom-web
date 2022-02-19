481ad2895133a97f9961ef5be75953a48c0a6396
Dominique Elvira <delviratapia@gmail.com>
2022-02-18 12:29:16 -1200

    Added sounds, icons and javascript to menu

--- src/main.js
+++ src/main.js
@@ -1,4 +1,43 @@
-// import { createApp } from 'vue'
-// import App from './App.vue'
-// import "./assets/styles.css"
-// createApp(App).mount('#app')
+const HOVERAUDIO = new Audio("src/assets/audio/rpg_hover-2.wav")
+let menuTextAudio = document.querySelectorAll(".cursor-pointer")
+for (let btn of menuTextAudio) {
+    btn.addEventListener("click", () => {
+        HOVERAUDIO.play()
+    })
+
+}
+
+
+
+let myItems = document.querySelector(".my-items")
+let portfTitle = document.querySelector(".portfolio-title")
+
+portfTitle.addEventListener("click", () => {
+    for (let item of myItems.children) {
+        if (item.classList.contains("portfolio-item")) {
+            item.classList.remove("hidden");
+        }
+        else {
+            if (!item.classList.contains("hidden")){
+                item.classList.add("hidden");
+            }
+
+        }
+    }
+})
+
+let cvTitle = document.querySelector(".cv-title")
+
+cvTitle.addEventListener("click", () => {
+    for (let item of myItems.children) {
+        if (item.classList.contains("cv-item")) {
+            item.classList.remove("hidden");
+        }
+        else {
+            if (!item.classList.contains("hidden")){
+                item.classList.add("hidden");
+            }
+
+        }
+    }
+})
