b3399b1b03b9c548617734a2bfb286274d91af15
Dominique Elvira <delviratapia@gmail.com>
2022-02-23 22:38:41 -1200

    - Added background to gameover file - Added buttons to gameover file - Added sound on click to "continue" button - Modified text size and spacing

--- src/gameover.js
+++ src/gameover.js
@@ -7,3 +7,15 @@ function onloadModal() {
     GAMEOVER_AUDIO.play()
 
 }
+
+
+const HOVER_AUDIO = new Audio("src/assets/audio/rpg_hover-2.wav")
+let gameoverBtn = document.querySelectorAll(".gameover-btn")
+
+
+for (let btn of gameoverBtn) {
+    btn.addEventListener("click", () => {
+        HOVER_AUDIO.play()
+    })
+
+}
