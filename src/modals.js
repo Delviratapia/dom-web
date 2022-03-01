b470b8e99d3abaf16d1671b357658a35882b92bc
Dominique Elvira <delviratapia@gmail.com>
2022-02-28 18:16:40 -1200

    - Finished display name on title with localStorage - Added cursor pointer to enter btn on intro modal - Input name cleared after submit

--- src/modals.js
+++ src/modals.js
@@ -12,29 +12,46 @@ function onloadModalInitial() {
 
 }
 
+// DISPLAY INPUT NAME ON TITLE
 
-let inputName = document.querySelector("input");
 let title = document.querySelector("title")
-inputName.onclick = function () {
-    title.innerHTML = inputName.value
+
+if (localStorage.getItem('name') !== null) {
+    title.innerHTML = localStorage.getItem("name")
 }
 
 
+let inputName = document.querySelector("input");
+let enterBtn = document.querySelector(".intro-modal-enter")
+enterBtn.addEventListener("click", () => {
+    localStorage.setItem("name",inputName.value)
+    title.innerHTML = localStorage.getItem("name")
+    inputName.value=""
+
+})
+
+
+
+
+
+
+// AUDIO WAITING TIME
 
 function play(url) {
     return new Promise(function (resolve, reject) { // return a promise
             var audio = new Audio(url); // create audio wo/ src
             audio.preload = "auto"; // intend to play through
-            audio.autoplay = true; // autoplay when loaded
+            audio.autoplay = true; // autoplay
             audio.onerror = reject; // on error, reject
             audio.onended = resolve; // when done, resolve
         
     });
 }
 
-let closeModalInitial = document.querySelector(".close-modal-Initial");
-closeModalInitial.onclick = function () {
-    // CONFIRM_AUDIO.play()
+
+
+
+enterBtn.onclick = function () {
     play("src/assets/audio/rpg_confirm.wav").then(()=>{
         modalInitial.style.display = "none";
         window.location.href = "./index.html"
