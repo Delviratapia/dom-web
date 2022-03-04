171480cc551e103ec7c9b0b1f886f6ce1a61db63
Dominique Elvira <delviratapia@gmail.com>
2022-03-03 13:22:30 -1200

    Added random funcionality to text messages for the beer item modal

--- src/itemsModal.js
+++ src/itemsModal.js
@@ -2,6 +2,15 @@ let STlife = document.querySelector(".stamina-life")
 let unknownItem = document.querySelector(".unknown-item")
 let riceItem = document.querySelector(".rice-item")
 let juiceItem = document.querySelector(".juice-item")
+
+let beerItemText = document.querySelector(".beer-item-text")
+
+let randomBeerText = 
+["I'm afraid I can't.",
+ "Sorry, I can't", 
+ "Not for me, thanks.", 
+ "I think I'll pass"]
+
 // MODAL BOX
 
 const TYPING_AUDIO = new Audio("src/assets/audio/TEXTtypingcut.mp3")
@@ -17,15 +26,15 @@ let openBeerModal = document.querySelector(".beer-item");
 let closeBeerModal = document.querySelector(".close-beer-modal");
 
 // When the user clicks the button, open the modal 
-openBeerModal.addEventListener("click", () =>{
+openBeerModal.addEventListener("click", () => {
     beerItem.style.display = "block";
+    beerItemText.innerHTML= "<p>" + '"' + randomBeerText[Math.floor(Math.random() * randomBeerText.length)] + '"' + "</p>";
     TYPING_AUDIO.play()
 })
 
 // When the user clicks on <span> (x), close the modal
-closeBeerModal.addEventListener("click", () =>{
+closeBeerModal.addEventListener("click", () => {
     beerItem.style.display = "none";
     TYPING_AUDIO.pause()
     CONFIRM_AUDIO.play()
 })
-
